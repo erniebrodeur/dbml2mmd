c0cc93d92c360322f863f3f21d7709176416a955
Ernie Brodeur <ebrodeur@ujami.net>
2025-02-26 20:16:39 -0800

    Add Slop dependency for CLI argument parsing; update tests for consistency

--- lib/dbml2mmd.rb
+++ lib/dbml2mmd.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'dbml'
+
 require 'dbml2mmd/version'
 require 'dbml2mmd/parser'
 require 'dbml2mmd/converter'
--- spec/dbml2mmd/cli_spec.rb
+++ spec/dbml2mmd/cli_spec.rb
@@ -19,13 +19,13 @@ RSpec.describe Dbml2Mmd::CLI do
   let(:args) { ['input.dbml'] }
 
   before do
-    allow(Slop::Options).to receive(:new).and.return(mock_parser)
+    allow(Slop::Options).to receive(:new).and_return(mock_parser)
     allow(mock_parser).to receive(:banner=)
     allow(mock_parser).to receive(:on)
-    allow(mock_parser).to receive(:parse).and.return(mock_opts)
-    allow(File).to receive(:read).and.return(simple_dbml)
+    allow(mock_parser).to receive(:parse).and_return(mock_opts)
+    allow(File).to receive(:read).and_return(simple_dbml)
     allow(File).to receive(:write)
-    allow(File).to receive(:exist?).and.return(true)
+    allow(File).to receive(:exist?).and_return(true)
   end
 
   describe '#run' do
@@ -33,18 +33,18 @@ RSpec.describe Dbml2Mmd::CLI do
       let(:args) { ['--help'] }
 
       before do
-        allow(mock_opts).to receive(:help?).and.return(true)
-        allow(mock_parser).to receive(:to_s).and.return('Usage help')
+        allow(mock_opts).to receive(:help).and_return(true)
+        allow(mock_parser).to receive(:to_s).and_return('Usage help')
       end
 
       it 'displays the help message' do
-        expect { cli.run }.to output(/Usage help/).to_stdout
+        expect { cli.run }.to output(/Usage help/).to_stdout.and raise_error(SystemExit)
       end
 
       it 'exits with status code 0' do
-        expect { cli.run }.to raise_error(SystemExit) { |error|
+        expect { cli.run }.to raise_error(SystemExit) do |error|
           expect(error.status).to eq(0)
-        }
+        end
       end
     end
 
@@ -52,8 +52,8 @@ RSpec.describe Dbml2Mmd::CLI do
       let(:args) { ['--version'] }
 
       before do
-        allow(mock_opts).to receive(:help?).and.return(false)
-        allow(mock_opts).to receive(:version?).and.return(true)
+        allow(mock_opts).to receive(:help).and_return(false)
+        allow(mock_opts).to receive(:version).and_return(true)
       end
 
       it 'displays the version' do
@@ -69,11 +69,11 @@ RSpec.describe Dbml2Mmd::CLI do
 
     context 'when processing an input file' do
       before do
-        allow(mock_opts).to receive(:help?).and.return(false)
-        allow(mock_opts).to receive(:version?).and.return(false)
-        allow(mock_opts).to receive(:[]).with(:output).and.return(nil)
-        allow(mock_opts).to receive(:[]).with(:theme).and.return(nil)
-        allow(mock_opts).to receive(:args).and.return(args)
+        allow(mock_opts).to receive(:help).and_return(false)
+        allow(mock_opts).to receive(:version).and_return(false)
+        allow(mock_opts).to receive(:[]).with(:output).and_return(nil)
+        allow(mock_opts).to receive(:[]).with(:theme).and_return(nil)
+        allow(mock_opts).to receive(:args).and_return(args)
       end
 
       it 'generates mermaid diagram syntax' do
@@ -85,11 +85,11 @@ RSpec.describe Dbml2Mmd::CLI do
       let(:args) { ['-o', 'output.mmd', 'input.dbml'] }
 
       before do
-        allow(mock_opts).to receive(:help?).and.return(false)
-        allow(mock_opts).to receive(:version?).and.return(false)
-        allow(mock_opts).to receive(:[]).with(:output).and.return('output.mmd')
-        allow(mock_opts).to receive(:[]).with(:theme).and.return(nil)
-        allow(mock_opts).to receive(:args).and.return(args)
+        allow(mock_opts).to receive(:help).and_return(false)
+        allow(mock_opts).to receive(:version).and_return(false)
+        allow(mock_opts).to receive(:[]).with(:output).and_return('output.mmd')
+        allow(mock_opts).to receive(:[]).with(:theme).and_return(nil)
+        allow(mock_opts).to receive(:args).and_return(args)
       end
 
       it 'writes output to the specified file' do
@@ -102,11 +102,11 @@ RSpec.describe Dbml2Mmd::CLI do
       let(:args) { ['--theme', 'dark', 'input.dbml'] }
 
       before do
-        allow(mock_opts).to receive(:help?).and.return(false)
-        allow(mock_opts).to receive(:version?).and.return(false)
-        allow(mock_opts).to receive(:[]).with(:output).and.return(nil)
-        allow(mock_opts).to receive(:[]).with(:theme).and.return('dark')
-        allow(mock_opts).to receive(:args).and.return(args)
+        allow(mock_opts).to receive(:help).and_return(false)
+        allow(mock_opts).to receive(:version).and_return(false)
+        allow(mock_opts).to receive(:[]).with(:output).and_return(nil)
+        allow(mock_opts).to receive(:[]).with(:theme).and_return('dark')
+        allow(mock_opts).to receive(:args).and_return(args)
       end
 
       it 'applies the specified theme' do
@@ -118,18 +118,18 @@ RSpec.describe Dbml2Mmd::CLI do
       let(:args) { ['non_existent.dbml'] }
 
       before do
-        allow(mock_opts).to receive(:help?).and.return(false)
-        allow(mock_opts).to receive(:version?).and.return(false)
-        allow(mock_opts).to receive(:[]).with(:output).and.return(nil)
-        allow(mock_opts).to receive(:[]).with(:theme).and.return(nil)
-        allow(mock_opts).to receive(:args).and.return(args)
-        allow(File).to receive(:exist?).with('non_existent.dbml').and.return(false)
+        allow(mock_opts).to receive(:help).and_return(false)
+        allow(mock_opts).to receive(:version).and_return(false)
+        allow(mock_opts).to receive(:[]).with(:output).and_return(nil)
+        allow(mock_opts).to receive(:[]).with(:theme).and_return(nil)
+        allow(mock_opts).to receive(:args).and_return(['non_existent.dbml'])
+        allow(File).to receive(:exist?).with('non_existent.dbml').and_return(false)
       end
 
       it 'displays an error message and exits with status 1' do
-        expect { cli.run }.to output(/Error: File not found/).to_stdout.and raise_error(SystemExit) { |error|
+        expect { cli.run }.to output(/Error: File not found/).to_stdout.and raise_error(SystemExit) do |error|
           expect(error.status).to eq(1)
-        }
+        end
       end
     end
   end
